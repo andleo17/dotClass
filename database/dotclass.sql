58d6e46d6acc2b506c429d50898b5f5bb9978c8e
Andres Baldarrago <andres.gastulo@gmail.com>
2019-10-12 22:43:09 -0500

    Terminando la tabla Usuario

--- database/dotclass.sql
+++ database/dotclass.sql
@@ -1,5 +1,26 @@
-CREATE TABLE USUARIO (
-    idUsuario              SERIAL              PRIMARY KEY,
-    nombreCompleto          varcahr(50)         NOT NULL,
-    fechaNacimiento         date                NOT NULL
+CREATE TABLE pais (
+    id                      SERIAL                  PRIMARY KEY,
+    nombre                  VARCHAR(20)             NOT NULL UNIQUE
+);
+
+CREATE TABLE ciudad (
+    id                      SERIAL                  PRIMARY KEY,
+    nombre                  VARCHAR(20)             NOT NULL UNIQUE
+);
+
+CREATE TABLE usuario (
+    id                      SERIAL                  PRIMARY KEY,
+    nickname                VARCHAR(30)             NOT NULL UNIQUE,
+    password                VARCHAR(30)             NOT NULL,
+    nombres                 VARCHAR(60)             NOT NULL,
+    apellidos               VARCHAR(60)             NOT NULL,
+    email                   VARCHAR(80)             NOT NULL,
+    fecha_nacimiento        DATE                    NOT NULL,
+    pais_id                 INT                     NOT NULL REFERENCES pais,
+    ciudad_id               INT                     NOT NULL REFERENCES ciudad,
+    descripcion             TEXT                    NULL,
+    numero_seguidores       INT                     NOT NULL DEFAULT 0,
+    pregunta_seguridad      TEXT                    NOT NULL,
+    respuesta_seguridad     VARCHAR(50)             NOT NULL,
+    foto                    VARCHAR(100)            NULL
 );
