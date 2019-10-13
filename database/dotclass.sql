c21dee08383a1004cd2abd13dffc8e08d7e53d1c
CinthyaYomona <54955856+CinthyaYomona@users.noreply.github.com>
2019-10-12 23:42:36 -0500

    Se agrego posibles tablas e ideas

--- database/dotclass.sql
+++ database/dotclass.sql
@@ -24,3 +24,34 @@ CREATE TABLE usuario (
     respuesta_seguridad     VARCHAR(50)             NOT NULL,
     foto                    VARCHAR(100)            NULL
 );
+
+CREATE TABLE conocimientos(
+    id                      SERIAL                  PRIMARY KEY,
+    nombre                  VARCHAR(100)            NOT NULL,
+    grado_academico         VARCHAR(40)             NULL,
+    lugar_estudio           VARCHAR(80)             NULL,
+    fecha                   INT                     NULL
+    pais_id                 INT                     NULL REFERENCES pais
+);
+
+/*POSIBLES TABLAS:
+    - Cursos
+    - Comentarios
+    - Experiencia_Laboral
+    - Movimientos (posible)
+    - pre - requisito
+    - clases
+    - aportes
+    - preguntas de examen
+    - proyecto_curso
+    - blog
+    - categoria
+    - 
+
+
+POSIBLES IDEAS:
+    - Secciones
+    - Examenes finales y detalle de examenes
+    - Marcadores
+    - Control de los vídeos
+*/
